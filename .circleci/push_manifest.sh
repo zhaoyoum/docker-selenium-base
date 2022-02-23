b88f02e73b92b9e258af8d738c46aa4ba2931c9e
zhaoyoum <52742741+zhaoyoum@users.noreply.github.com>
2022-02-23 18:29:55 +0800

    Update push_manifest.sh

--- .circleci/push_manifest.sh
+++ .circleci/push_manifest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Downloading manifest-tool."
-MT_VERSION=$(curl https://api.github.com/repos/estesp/manifest-tool/releases | jq '.[].tag_name' -r | head -n1)
+#MT_VERSION=$(curl https://api.github.com/repos/estesp/manifest-tool/releases | jq '.[].tag_name' -r | head -n1)
+MT_VERSION=v1.0.3
 curl -O -sL https://github.com/estesp/manifest-tool/releases/download/${MT_VERSION}/manifest-tool-linux-arm64
 mv manifest-tool-linux-arm64 /usr/bin/manifest-tool
 chmod +x /usr/bin/manifest-tool
