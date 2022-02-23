fbd65565a4a51e95155564e4ea5f406660a58fee
zhaoyoum <52742741+zhaoyoum@users.noreply.github.com>
2022-02-23 15:46:16 +0800

    Add files via upload

--- .circleci/push_manifest.sh
+++ .circleci/push_manifest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo "Downloading manifest-tool."
 MT_VERSION=$(curl https://api.github.com/repos/estesp/manifest-tool/releases | jq '.[].tag_name' -r | head -n1)
-curl -O -sL https://github.com/estesp/manifest-tool/releases/download/${MT_VERSION}/manifest-tool-linux-mips64el
-mv manifest-tool-linux-mips64 /usr/bin/manifest-tool
+curl -O -sL https://github.com/estesp/manifest-tool/releases/download/${MT_VERSION}/manifest-tool-linux-arm64
+mv manifest-tool-linux-arm64 /usr/bin/manifest-tool
 chmod +x /usr/bin/manifest-tool
 manifest-tool --version
 
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
 manifest-tool push from-args \
-  --platforms linux/mips64el \
+  --platforms linux/arm64 \
   --template "$REGISTRY/$IMAGE:$VERSION-ARCH" \
   --target "$REGISTRY/$IMAGE:$VERSION"
 if [ "${CIRCLE_BRANCH}" == 'master' ]; then
   manifest-tool push from-args \
-    --platforms linux/mips64el \
+    --platforms linux/arm64 \
     --template "$REGISTRY/$IMAGE:$VERSION-ARCH" \
     --target "$REGISTRY/$IMAGE:latest"
 fi
