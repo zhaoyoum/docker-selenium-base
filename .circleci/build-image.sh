f7b2c71b53f692cfb9f5172b88dfa3f90e4895fc
zhaoyoum <52742741+zhaoyoum@users.noreply.github.com>
2022-02-24 15:01:55 +0800

    Update build-image.sh

--- .circleci/build-image.sh
+++ .circleci/build-image.sh
@@ -10,6 +10,8 @@ if [ $GOARCH == 'amd64' ]; then
   touch qemu-amd64-static
 else
   curl -sL "https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/qemu-${QEMU_ARCH}-static.tar.gz" | tar xz
+  cp qemu-${QEMU_ARCH}-static /usr/bin/
+  chmod +x /usr/bin/qemu-${QEMU_ARCH}-static
   docker run --rm --privileged multiarch/qemu-user-static:register
 fi
 # </qemu-support>
